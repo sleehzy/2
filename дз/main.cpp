74b11ed67a2625be7d8e71fc44a815526fdebd24
валера загрутдинов <zagrutdinov07@mail.ru>
2026-02-03 19:03:42 +0500

    регистрация

--- дз/main.cpp
+++ дз/main.cpp
@@ -1,82 +1,160 @@
+#include <Windows.h>
+#include <type_traits>
+#include <string>
 #include <iostream>
-using namespace std;
-bool vys(int year)
+//using namespace std;
+//bool vys(int year)
+//{
+//    bool res = false;
+//    if (year % 4 == 0)
+//        res = true;
+//    if (year % 100 == 0)
+//        res = false;
+//    if (year % 400 == 0)
+//        res = true;
+//    return res;
+//}
+//int date(int d, int m, int y)
+//{
+//    int k = d;
+//    if (vys(y) && (m > 2))
+//        k += y * 366;
+//    else k += y * 365;
+//    switch (m - 1)
+//    {
+//    case 12: k += 31;
+//    case 11: k += 30;
+//    case 10: k += 31;
+//    case  9: k += 30;
+//    case  8: k += 31;
+//    case  7: k += 31;
+//    case  6: k += 30;
+//    case  5: k += 31;
+//    case  4: k += 30;
+//    case  3: k += 31;
+//    case  2: k += 28;
+//    case  1: k += 31;
+//    }
+//    return k;
+//}
+//int difference(int d1, int d2, int m1, int m2, int y1, int y2)
+//{
+//    int k = date(d2, m2, y2) - date(d1, m1, y1);
+//    return k;
+//}
+//
+//double average(int a[], int n)
+//{
+//    // Find sum of array element
+//    int sum = 0;
+//    for (int i = 0; i < n; i++)
+//        sum += a[i];
+//
+//    return (double)sum / n;
+//}
+
+
+
+std::string login, password, loginInput, passwordInput;
+int loginMin = 3, loginMax = 15, passMin = 8, passMax = 30;
+
+bool PassDigit(const std::string& currentPass)
 {
-    bool res = false;
-    if (year % 4 == 0)
-        res = true;
-    if (year % 100 == 0)
-        res = false;
-    if (year % 400 == 0)
-        res = true;
-    return res;
-}
-int date(int d, int m, int y)
-{
-    int k = d;
-    if (vys(y) && (m > 2))
-        k += y * 366;
-    else k += y * 365;
-    switch (m - 1)
+    int digitCount = 0;
+    for (char c : currentPass)
     {
-    case 12: k += 31;
-    case 11: k += 30;
-    case 10: k += 31;
-    case  9: k += 30;
-    case  8: k += 31;
-    case  7: k += 31;
-    case  6: k += 30;
-    case  5: k += 31;
-    case  4: k += 30;
-    case  3: k += 31;
-    case  2: k += 28;
-    case  1: k += 31;
+        if (std::isdigit(c))
+        {
+            digitCount++;
+            if (digitCount >= 3)
+                return true;
+        }
     }
-    return k;
+    return false;
 }
-int difference(int d1, int d2, int m1, int m2, int y1, int y2)
+
+bool PassChar(const std::string& currentPass)
 {
-    int k = date(d2, m2, y2) - date(d1, m1, y1);
-    return k;
+    int letterCount = 0;
+    for (char c : currentPass)
+    {
+        if (std::isalpha(c))
+        {
+            letterCount++;
+            if (letterCount >= 3)
+                return true;
+        }
+    }
+    return false;
 }
-
-double average(int a[], int n)
-{
-    // Find sum of array element
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-        sum += a[i];
-
-    return (double)sum / n;
-}
-
-
 
 int main()
 {
-    setlocale(LC_ALL,"ru");
-    std::cout << "\t\t\t\tпервое задание: \n\n\n\n\n";
-    int d1, d2, m1, m2, y1, y2;
-    cout << "1 день: ";
-    cin >> d2;
-    cout << "1 месяц: ";
-    cin >> m2;
-    cout << "1 год: ";
-    cin >> y2;
-    cout << "2 день: ";
-    cin >> d1;
-    cout << "2 месяц: ";
-    cin >> m1;
-    cout << "2 год: ";
-    cin >> y1;
-    cout << "различия между этими двумя датами " << difference(d1, d2, m1, m2, y1, y2) << " дней\n";
-    system("PAUSE");
 
-    std::cout << "\t\t\t\tвторое задание: \n\n\n\n\n";
-    int arr[] = { 10, 2, 3, 4, 5, 6, 7, 8, 9 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    while (true)
+    {
+        std::cout << "Введите новый логин: ";
+        std::getline(std::cin, login, '\n');
 
-    cout << average(arr, n);
+        std::cout << "Введите новый пароль: ";
+        std::getline(std::cin, password, '\n');
+
+        if (login.size() < loginMin || login.size() > loginMax)
+        {
+            std::cout << "Недопустимая длина логина. Должно быть от "
+                << loginMin << " до " << loginMax << '\n\n';
+            Sleep(1500);
+            system("cls");
+        }
+        else if (password.size() < passMin || password.size() > passMax)
+        {
+            std::cout << "Недопустимая длина пароля. Должно быть от "
+                << passMin << " до " << passMax << '\n\n';
+            Sleep(1500);
+            system("cls");
+        }
+        else
+        {
+            if (PassDigit(password) && PassChar(password))
+            {
+                std::cout << "Регистрация прошла успешно";
+                break;
+            }
+            else
+            {
+                std::cout << "Ошибка: пароль должен содержать минимум 3 цифры и 3 буквы.\n";
+                Sleep(1500);
+                system("cls");
+            }
+        }
+    }
+
+    Sleep(1500);
+    system("cls");
+
+   
+    while (true)
+    {
+        std::cout << "Введите ваш логин: ";
+        std::getline(std::cin, loginInput, '\n');
+
+        std::cout << "Введите ваш пароль: ";
+        std::getline(std::cin, passwordInput, '\n');
+
+        if (login == loginInput && password == passwordInput)
+        {
+            std::cout << "Авторизация прошла успешно! Добро пожаловать! \n\n";
+            Sleep(1500);
+            system("cls");
+            break;
+        }
+        std::cout << "Ошибка логина или пароля \n";
+        Sleep(1500);
+        system("cls");
+    }
 
     return 0;
 }
